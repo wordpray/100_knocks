305efcd94df6576659a1127d08d2aa04d92df23b
y.yoshiki <labiatejoker@gmail.com>
2017-09-06 16:25:09 +0900

    Create aswer

---
+++ 28. MediaWikiマークアップの除去.rb
@@ -0,0 +1,47 @@
+basic_info = Hash.new
+
+open('jawiki-country.json', 'r') do |input|
+  flag = false
+  key = ''
+  while line = input.gets
+    line.split('\n').each do |item|
+      (flag = true; next) if item.match(/基礎情報/)
+      flag = false if item.match(/^}}/)
+      if flag
+        if item =~ /^\*/
+          basic_info[key] += item
+        else
+          raw = item.split('=')
+          key = raw[0].sub(/^\|/,'').rstrip
+          basic_info[key] = raw[1].strip#.gsub(/\<.*?>/,'')
+        end
+      end
+    end
+  end
+end
+
+def remove_emphasized_link(basic_info)
+  basic_info_removed_emphasized_link = Hash.new
+  basic_info.each do |k, v|
+    basic_info_removed_emphasized_link[k] = v.gsub(/\'+/, ' ')
+  end
+  return basic_info_removed_emphasized_link
+end
+
+def remove_internal_link(basic_info_removed_emphasized_link)
+  basic_info_removed_internal_link = Hash.new
+  basic_info_removed_emphasized_link.each do |k, v|
+    basic_info_removed_internal_link[k] = v.gsub(/\[+/, ' ').gsub(/\]+/, ' ')
+  end
+  return basic_info_removed_internal_link
+end
+
+def remove_other_link(basic_info_removed_internal_link)
+  basic_info_removed_other_link = Hash.new
+  basic_info_removed_internal_link.each do |k, v|
+    basic_info_removed_other_link[k] = v.gsub(/\{+/, ' ').gsub(/\}+/, ' ')
+  end
+  return basic_info_removed_other_link
+end
+
+puts remove_other_link(remove_internal_link(remove_emphasized_link(basic_info)))
