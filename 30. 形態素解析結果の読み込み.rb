8e4df4e4baa4d431a20760731b1eafc207bcc906
y.yoshiki <labiatejoker@gmail.com>
2017-09-11 13:53:19 +0900

    Create answer

---
+++ 30. 形態素解析結果の読み込み.rb
@@ -0,0 +1,20 @@
+require "natto"
+
+def morphize
+  list = Array.new
+  open("neko.txt", "r") do |input|
+    sentense = Array.new
+    while line = input.gets
+      if line =~ /EOS/
+        list.push(sentense) unless sentense.empty?
+        sentense = Array.new
+      next
+      end
+      item = line.chomp.split(/\t/)
+      surface = item[0]
+      block = item[1].split(',')
+      sentense.push({surface: surface, base: block[6], pos: block[0], pos1: block[1]})
+    end
+  end
+  return list
+end
