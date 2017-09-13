f449bcc8670bbc24640f76fabf941761297d4a03
y.yoshiki <labiatejoker@gmail.com>
2017-09-13 13:04:47 +0900

    Create answer

--- 01. 「パタトクカシーー」.rb
+++ 01. 「パタトクカシーー」.rb
@@ -1,1 +1,17 @@
 str.length.times{ |n| puts str[n] if n.even?}
+
+japa,math, his, scie
+1,2,3,4
+1,2,3,4
+1,2,3,4
+1,2,3,4
+
+
+require 'csv'
+CSV.new(ARGF.file).each do |row|
+  puts row
+  jap= row
+  math=
+  his= 
+  sci= 
+end
--- 03. 円周率.rb
+++ 03. 円周率.rb
@@ -1,3 +1,13 @@
 str = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
 
-p str.gsub(/,|\./,'').split.map{ |s| s.length }
+str = gets.chomp.to_s
+
+uppercase = []
+num = []
+
+# p str.gsub(/,|\./,'').split.map{ |s| s.length }
+
+str.gsub(/,|\./,'').split.map{ |s| uppercase << s if s =~ /^[A-Z]/ }
+str.gsub(/,|\./,'').split.map{ |s| num << s if s =~ /^[0-9]/ }
+
+puts uppercase.count + num.count
