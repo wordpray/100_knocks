36aedad11fa8de3f30dee01b9e5701398a498054
y.yoshiki <labiatejoker@gmail.com>
2017-10-02 14:56:45 +0900

    Create answer

---
+++ 40. 係り受け解析結果の読み込み.rb
@@ -0,0 +1,36 @@
+require 'natto'
+
+class Morph
+  def initialize(morph)
+    @surface, block = morph.split(/\t/)
+    parse(block)
+  end
+  def parse(block)
+    block.split(/,/).each_with_index do |item, index|
+      if index == 0 || index == 1 || index == 6
+        @pos1 = item if index == 0
+        @pos2 = item if index == 1
+        @base = item if index == 6
+      end
+    end
+  end
+end
+
+list = []
+natto = Natto::MeCab.new
+
+open('../data/neko.txt.cabocha', 'r') do |input|
+  arr = []
+  while line = input.gets
+    line = line.chomp.gsub(/\-|D|\s|\|/, '')
+    natto.parse(line).split(/\n/).each do |morph|
+      next if morph =~ /^EOS|空白/
+        arr.push(Morph.new(morph))
+      (list.push(arr) if arr.size != 0; arr = []; next) if morph =~ /。/
+    end
+  end
+end
+
+if $0 == __FILE__
+  p list[2]
+end
